f63323fc54422688d215d401c1c0c6443029b0c2
munyamugb <munyaradzi045@gmail.com>
2023-12-06 13:50:47 +0200

    Add about infor

---
+++ send_mail.php
@@ -0,0 +1,20 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    // Compose the email message
+    $to = 'chimwangoconfidence@gmail.com';
+    $subject = 'New Contact Form Submission';
+    $body = "Full Name: $fullname\nEmail: $email\nPhone Number: $phone\nMessage: $message";
+
+    // Send the email
+    if (mail($to, $subject, $body)) {
+        echo '<script>alert("Message sent successfully. We will contact you shortly.");</script>';
+    } else {
+        echo '<script>alert("Oops! Something went wrong. Please try again later.");</script>';
+    }
+    }
+?>
