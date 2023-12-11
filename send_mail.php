e81ea88f1ed1275a8baafef273fdc440eb8b962c
munyamugb <munyaradzi045@gmail.com>
2023-12-11 11:41:34 +0200

    Contact styles

--- send_mail.php
+++ send_mail.php
@@ -1,20 +1,55 @@
 <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $fullname = $_POST['fullname'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $message = $_POST['message'];
 
-    // Compose the email message
-    $to = 'munyaradzi045@gmail.com';
-    $subject = 'New Contact Form Submission';
-    $body = "Full Name: $fullname\nEmail: $email\nPhone Number: $phone\nMessage: $message";
+//Add your information here
+$recipient = "lespakhydraulics@gmail.com";
 
-    // Send the email
-    if (mail($to, $subject, $body)) {
-        echo '<script>alert("Message sent successfully. We will contact you shortly.");</script>';
-    } else {
-        echo '<script>alert("Oops! Something went wrong. Please try again later.");</script>';
-    }
-    }
-?>
+//Don't edit anything below this line
+
+//import form information
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+
+$name=stripslashes($name);
+$email=stripslashes($email);
+$phone=stripslashes($phone);
+$message=stripslashes($message);
+$message= "Name: $name, Phone: $phone \n\n Message: $message";
+
+/*
+Simple form validation
+check to see if an email and message were entered
+*/
+
+//if no message entered and no email entered print an error
+if (empty($message) && empty($email)){
+print "No email address and no message was entered. <br>Please include an email and a message";
+}
+//if no message entered send print an error
+elseif (empty($message)){
+print "No message was entered.<br>Please include a message.<br>";
+}
+//if no email entered send print an error
+elseif (empty($email)){
+print "No email address was entered.<br>Please include your email. <br>";
+}
+
+
+//mail the form contents
+if(mail("$recipient", "$phone", "$message", "From: $email" )) {
+
+	// Email has sent successfully, echo a success page.
+
+	echo '<div class="alert alert-success alert-dismissable fade in">
+		<button type = "button" class = "close" data-dismiss = "alert" aria-hidden = "true">&times;</button>
+    
+		<p>Email Sent Successfully! We Will get back to you shortly</p></div>';
+
+	} else {
+
+	echo 'ERROR!';
+
+	}
+
